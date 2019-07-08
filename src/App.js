4719d4997558996f4ef4a9d049e5ffecfbe896f4
Nick <nicolasmontoya@yahoo.com>
2019-07-08 00:01:24 -0600

    first commit on minimal branch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React, { useState, useEffect, Fragment } from 'react';
 import Amplify, { Auth, API, graphqlOperation } from 'aws-amplify';
-import awsconfig from './aws-exports';
-import IdleTimer from 'react-idle-timer';
-import { MDBContainer, MDBRow, MDBCol, MDBFooter, MDBBtn } from 'mdbreact';
-import AWSAppSyncClient, { AUTH_TYPE } from 'aws-appsync';
+
 import {
   withAuthenticator,
   AmplifyTheme,
