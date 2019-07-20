e507687da82bd45d01e89f5a221f463275e31aa5
Nick <nicolasmontoya@yahoo.com>
2019-07-20 14:07:53 -0600

    first commit

--- src/views/Settings.js
+++ src/views/Settings.js
@@ -3,40 +3,54 @@ import { withRouter } from 'react-router-dom';
 import { MDBContainer, MDBRow, MDBCol, MDBBtn } from 'mdbreact';
 import { useStateValue } from '../state/StateManagement';
 
-const Settings = (props) => {
-    const globalStore = useStateValue()[0];
+const Settings = props => {
+  const globalStore = useStateValue()[0];
 
-    const locations = globalStore.franchiseLocations.length > 0 ?
-        globalStore.franchiseLocations.map(eachLocation =>
-            (<MDBRow>{eachLocation.franchise + ": " + eachLocation.location}</MDBRow>)) : <MDBRow>None</MDBRow>;
-    const units = globalStore.unitOptions.length > 0 ?
-        globalStore.unitOptions.map(unit => (<MDBRow>{unit.label}</MDBRow>)) : <MDBRow>None</MDBRow>;
-    const brands = globalStore.brandOptions.length > 0 ?
-        globalStore.brandOptions.map(brand => (<MDBRow>{brand}</MDBRow>)) : <MDBRow>None</MDBRow>;
-
-    return (
-        <MDBContainer
-            style={{ paddingTop: '50px', paddingBottom: '50px' }}>
-            <MDBRow>
-                <strong>Settings</strong>
-            </MDBRow>
-            <MDBCol>
-                <MDBRow>User Information</MDBRow>
-                <MDBCol><MDBRow>{"Email: " + globalStore.userEmail}</MDBRow></MDBCol>
-                <MDBRow>Franchise Locations</MDBRow>
-                <MDBCol>{locations}</MDBCol>
-                <MDBRow>Brands</MDBRow>
-                <MDBCol>{brands}</MDBCol>
-                <MDBRow>Units</MDBRow>
-                <MDBCol>{units}</MDBCol>
-            </MDBCol>
-            <MDBRow>
-                <MDBBtn color="back" rounded onClick={() => props.history.goBack()}>
-                    Back
-                </MDBBtn>
-            </MDBRow>
-        </MDBContainer>
+  const locations =
+    globalStore.franchiseLocations.length > 0 ? (
+      globalStore.franchiseLocations.map(eachLocation => (
+        <MDBRow>{eachLocation.franchise + ': ' + eachLocation.location}</MDBRow>
+      ))
+    ) : (
+      <MDBRow>None</MDBRow>
     );
-}
+  const units =
+    globalStore.unitOptions.length > 0 ? (
+      globalStore.unitOptions.map(unit => <MDBRow>{unit.label}</MDBRow>)
+    ) : (
+      <MDBRow>None</MDBRow>
+    );
+  const brands =
+    globalStore.brandOptions.length > 0 ? (
+      globalStore.brandOptions.map(brand => <MDBRow>{brand}</MDBRow>)
+    ) : (
+      <MDBRow>None</MDBRow>
+    );
+
+  return (
+    <MDBContainer style={{ paddingTop: '50px', paddingBottom: '50px' }}>
+      <MDBRow>
+        <strong>Settings</strong>
+      </MDBRow>
+      <MDBCol>
+        <MDBRow>User Information</MDBRow>
+        <MDBCol>
+          <MDBRow>{'Email: ' + globalStore.userEmail}</MDBRow>
+        </MDBCol>
+        <MDBRow>Franchise Locations</MDBRow>
+        <MDBCol>{locations}</MDBCol>
+        <MDBRow>Brands</MDBRow>
+        <MDBCol>{brands}</MDBCol>
+        <MDBRow>Units</MDBRow>
+        <MDBCol>{units}</MDBCol>
+      </MDBCol>
+      <MDBRow>
+        <MDBBtn color="back" rounded onClick={() => props.history.goBack()}>
+          Back
+        </MDBBtn>
+      </MDBRow>
+    </MDBContainer>
+  );
+};
 
 export default withRouter(Settings);
